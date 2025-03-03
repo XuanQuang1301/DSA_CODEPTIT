2eed485f018b158307c158ef169835d1c8149120
Xuân Quang <quangxuan1301@gmail.com>
2025-03-04 01:05:55 +0700

    Update DSA06010.cpp

--- DSA06010.cpp
+++ DSA06010.cpp
@@ -4,6 +4,17 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        
+        int n;
+        cin >> n;
+        set<char> SE;
+        for(int i = 0; i < n; i++){
+            string s;
+            cin >> s;
+            for(int j = 0; j < s.size(); j++){
+                SE.insert(s[j]);
+            }
+        }
+        for(auto i : SE) cout << i << " ";
+        cout << endl;
     }
 }
